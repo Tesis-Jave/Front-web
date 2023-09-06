666eb2b438f97c7b69fc65b6c9b18ca2ed89fd00
mejiarocha <70533112+mejiarocha@users.noreply.github.com>
2023-09-05 20:05:40 -0500

    Estructura del proyecto
    Carpetas de componentes y modelo para la estructuracion del proyecto

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -1,18 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/components/home/home.component';
+import { ProductoComponent } from './components/productos/producto/producto.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    ProductoComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
+
+
   ],
   providers: [],
   bootstrap: [AppComponent]
--- front/src/app/models/productos/producto.ts
+++ front/src/app/models/productos/producto.ts
@@ -1,2 +1,15 @@
 export class Producto {
+    constructor(
+         // this.id = 0;
+        // this.nombre = '';
+        // this.descripcion = '';
+        // this.precio = 0;
+        // this.stock = 0;
+        public id: Number,
+        public nombre: String,
+        public descripcion: String,
+        public precio: Number,
+        public stock: Number,
+        
+    ) {}
 }
